8db65f61725ab1a262aca716825c7836ed981f74
Purox <ilikepurox@web.de>
2018-06-21 05:03:17 +0200

    new respoinse waiter on automod (except for roles)

--- src/main/java/me/purox/devi/commands/guild/handler/AutoModAdsHandler.java
+++ src/main/java/me/purox/devi/commands/guild/handler/AutoModAdsHandler.java
@@ -3,11 +3,8 @@ package me.purox.devi.commands.guild.handler;
 import me.purox.devi.commands.handler.Command;
 import me.purox.devi.commands.handler.CommandSender;
 import me.purox.devi.core.Devi;
-import me.purox.devi.core.DeviEmote;
 import me.purox.devi.core.guild.GuildSettings;
-import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
-
-import java.util.concurrent.TimeUnit;
+import me.purox.devi.core.waiter.WaitingResponseBuilder;
 
 public class AutoModAdsHandler {
 
@@ -17,58 +14,11 @@ public class AutoModAdsHandler {
     }
 
     public void handle(Command command, CommandSender sender) {
-
-        String builder = DeviEmote.INFO.get() + " | " + devi.getTranslation(command.getLanguage(), 266) + " -> " + devi.getTranslation(command.getLanguage(), 267) + "\n\n" +
-                "```python\n" +
-                devi.getTranslation(command.getLanguage(), 268) + "\n\n" +
-                " '1' => " + devi.getTranslation(command.getLanguage(), 269) + "\n" +
-                " '2' => " + devi.getTranslation(command.getLanguage(), 270) + "\n" +
-                "```\n" + devi.getTranslation(command.getLanguage(), 271, "`cancel`");
-
-        sender.reply(builder);
-
-        startWaiter(1, command, sender);
-    }
-
-    private void startWaiter(int attempt, Command command, CommandSender sender) {
-        int nextAttempt = attempt += 1;
-        MessageReceivedEvent event = command.getEvent();
-        devi.getResponseWaiter().waitForResponse(event.getGuild(),
-                evt -> devi.getResponseWaiter().checkUser(evt, event.getMessageId(), event.getAuthor().getId(), event.getChannel().getId()),
-                response -> {
-                    if (response.getMessage().getContentRaw().toLowerCase().startsWith("cancel")) {
-                        sender.reply(DeviEmote.ERROR.get() + " | " + devi.getTranslation(command.getLanguage(), 262));
-                        return;
-                    }
-
-                    if (nextAttempt >= 4) {
-                        sender.reply(DeviEmote.ERROR.get() + " | " + devi.getTranslation(command.getLanguage(), 279));
-                        return;
-                    }
-
-                    String input = response.getMessage().getContentRaw().split(" ")[0];
-
-                    int entered;
-                    try {
-                        entered = Integer.parseInt(input);
-                    } catch (NumberFormatException e) {
-                        entered = -1;
-                    }
-
-                    if (entered > 2 || entered < 1) {
-                        sender.reply(DeviEmote.ERROR.get() + " | " + devi.getTranslation(command.getLanguage(), 265));
-                        startWaiter(nextAttempt, command, sender);
-                        return;
-                    }
-
-                    command.getDeviGuild().getSettings().setBooleanValue(GuildSettings.Settings.AUTO_MOD_ANTI_ADS, entered == 1);
-                    command.getDeviGuild().saveSettings();
-
-                    if (entered == 1)
-                        sender.reply(DeviEmote.SUCCESS.get() + " | " + devi.getTranslation(command.getLanguage(), 280));
-                    else sender.reply(DeviEmote.SUCCESS.get() + " | " + devi.getTranslation(command.getLanguage(), 281));
-                },
-                15, TimeUnit.SECONDS, () -> sender.reply(DeviEmote.ERROR.get() + " | " + devi.getTranslation(command.getLanguage(), 263)) );
+        new WaitingResponseBuilder(devi, command)
+                .setExpectedInputText(devi.getTranslation(command.getLanguage(), 422))
+                .setWaiterType(WaitingResponseBuilder.WaiterType.BOOLEAN)
+                .setSetting(GuildSettings.Settings.AUTO_MOD_ANTI_ADS)
+                .build().handle();
     }
 }
 

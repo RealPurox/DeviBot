9b3349fc451706e489e9062385e34b1b3896b55b
Leo <arrayofc@gmail.com>
2018-07-29 00:06:50 +0200

    fix
    fix

--- src/main/java/me/purox/devi/commands/general/WeatherCommandExecutor.java
+++ src/main/java/me/purox/devi/commands/general/WeatherCommandExecutor.java
@@ -55,121 +55,122 @@ public class WeatherCommandExecutor implements CommandExecutor {
 
             List<Channel> list = limit.first(1);
             Channel city = list.get(0);
+            if(city != null) {
+                //wind
+                direction = city.getWind().getDirection() == null ? "Couldn't grab data." : city.getWind().getDirection() + "°";
+                speed = city.getWind().getSpeed() + "km/h";
+                wind = devi.getTranslation(command.getLanguage(), 378) + ": " + direction + "\n" + devi.getTranslation(command.getLanguage(), 379) + ": " + speed;
 
-            //wind
-            direction = city.getWind().getDirection() + "°";
-            speed = city.getWind().getSpeed() + "km/h";
-            wind = devi.getTranslation(command.getLanguage(), 378) + ": " + direction + "\n" + devi.getTranslation(command.getLanguage(), 379) + ": " + speed;
+                //Atmosphere
+                humidity = city.getAtmosphere().getHumidity() + "%";
+                double pRound = city.getAtmosphere().getPressure();
+                pressure = String.format("%.0f", pRound) + " psi";
+                visibility = city.getAtmosphere().getVisibility() / 100 + " miles";
 
-            //Atmosphere
-            humidity = city.getAtmosphere().getHumidity() + "%";
-            double pRound = city.getAtmosphere().getPressure();
-            pressure = String.format("%.0f", pRound) + " psi";
-            visibility = city.getAtmosphere().getVisibility() / 100 + " miles";
+                //Conditions
+                conditions = city.getItem().getCondition().getText();
+                temp = city.getItem().getCondition().getTemp() + "°C";
+                date = city.getItem().getCondition().getDate().toString();
 
-            //Conditions
-            conditions = city.getItem().getCondition().getText();
-            temp = city.getItem().getCondition().getTemp() + "°C";
-            date = city.getItem().getCondition().getDate().toString();
+                //Temp in fahrenheit
+                double temper = (double) Math.round((city.getItem().getCondition().getTemp() * 1.8 + 32) * 100) / 100;
+                tempF = temper + "°F";
 
-            //Temp in fahrenheit
-            double temper = (double) Math.round((city.getItem().getCondition().getTemp() * 1.8 + 32) * 100) / 100;
-            tempF = temper + "°F";
+                EmbedBuilder embed = new EmbedBuilder();
 
-            EmbedBuilder embed = new EmbedBuilder();
+                switch (city.getItem().getCondition().getCode()) {
 
-            switch (city.getItem().getCondition().getCode()){
+                    case 29: // partly cloudy night
+                        embed.setColor(Color.decode("#cccccc"));
+                        break;
+                    case 30: // partly cloudy day
+                        embed.setColor(Color.decode("#cccccc"));
+                        break;
+                    case 27: //mostly cloudy night
+                        embed.setColor(Color.decode("#bfbfbf"));
+                        break;
+                    case 28: //mostly clody day
+                        embed.setColor(Color.decode("#bfbfbf"));
+                        break;
+                    case 26: // clody
+                        embed.setColor(Color.decode("#a6a6a6"));
+                        break;
+                    case 31: //clear night
+                        embed.setColor(Color.cyan);
+                        break;
+                    case 32: // sunny
+                        embed.setColor(Color.yellow);
+                        break;
+                    case 24: //windy
+                        embed.setColor(Color.decode("#bfbfbf"));
+                        break;
+                    case 36: // hot
+                        embed.setColor(Color.orange);
+                        break;
+                    case 2: // rip i forgot all thses :_:
+                        embed.setColor(Color.decode("#737373"));
+                        break;
+                    case 3:
+                        embed.setColor(Color.decode("#404040"));
+                        break;
+                    case 4:
+                        embed.setColor(Color.decode("#404040"));
+                        break;
+                    case 5:
+                        embed.setColor(Color.decode("#ccffff"));
+                        break;
+                    case 8:
+                        embed.setColor(Color.decode("#00cccc"));
+                        break;
+                    case 9:
+                        embed.setColor(Color.decode("#0080ff"));
+                        break;
+                    case 10:
+                        embed.setColor(Color.decode("#00cccc"));
+                        break;
+                    case 11:
+                        embed.setColor(Color.decode("#0066cc"));
+                        break;
+                    case 12:
+                        embed.setColor(Color.decode("#0066cc"));
+                        break;
+                    case 13:
+                        embed.setColor(Color.white);
+                        break;
+                    case 14:
+                        embed.setColor(Color.white);
+                        break;
+                    case 15:
+                        embed.setColor(Color.white);
+                        break;
+                    case 41:
+                        embed.setColor(Color.white);
+                        break;
+                    case 42:
+                        embed.setColor(Color.white);
+                        break;
+                    case 43:
+                        embed.setColor(Color.white);
+                        break;
+                }
 
-                case 29: // partly cloudy night
-                    embed.setColor(Color.decode("#cccccc"));
-                    break;
-                case 30: // partly cloudy day
-                    embed.setColor(Color.decode("#cccccc"));
-                    break;
-                case 27: //mostly cloudy night
-                    embed.setColor(Color.decode("#bfbfbf"));
-                    break;
-                case 28: //mostly clody day
-                    embed.setColor(Color.decode("#bfbfbf"));
-                    break;
-                case 26: // clody
-                    embed.setColor(Color.decode("#a6a6a6"));
-                    break;
-                case 31: //clear night
-                    embed.setColor(Color.cyan);
-                    break;
-                case 32: // sunny
-                    embed.setColor(Color.yellow);
-                    break;
-                case 24: //windy
-                    embed.setColor(Color.decode("#bfbfbf"));
-                    break;
-                case 36: // hot
-                    embed.setColor(Color.orange);
-                    break;
-                case 2:
-                    embed.setColor(Color.decode("#737373"));
-                    break;
-                case 3:
-                    embed.setColor(Color.decode("#404040"));
-                    break;
-                case 4:
-                    embed.setColor(Color.decode("#404040"));
-                    break;
-                case 5:
-                    embed.setColor(Color.decode("#ccffff"));
-                    break;
-                case 8:
-                    embed.setColor(Color.decode("#00cccc"));
-                    break;
-                case 9:
-                    embed.setColor(Color.decode("#0080ff"));
-                    break;
-                case 10:
-                    embed.setColor(Color.decode("#00cccc"));
-                    break;
-                case 11:
-                    embed.setColor(Color.decode("#0066cc"));
-                    break;
-                case 12:
-                    embed.setColor(Color.decode("#0066cc"));
-                    break;
-                case 13:
-                    embed.setColor(Color.white);
-                    break;
-                case 14:
-                    embed.setColor(Color.white);
-                    break;
-                case 15:
-                    embed.setColor(Color.white);
-                    break;
-                case 41:
-                    embed.setColor(Color.white);
-                    break;
-                case 42:
-                    embed.setColor(Color.white);
-                    break;
-                case 43:
-                    embed.setColor(Color.white);
-                    break;
+
+                embed.setAuthor(devi.getTranslation(command.getLanguage(), 380, city.getTitle().substring(17)));
+                embed.setFooter(devi.getTranslation(command.getLanguage(), 380, city.getTitle().substring(17)) + " | " + date, null);
+
+                embed.addField(devi.getTranslation(command.getLanguage(), 381), temp + "/" + tempF, true);
+                embed.addField(devi.getTranslation(command.getLanguage(), 382), wind, true);
+                embed.addField(devi.getTranslation(command.getLanguage(), 383), humidity, true);
+                embed.addField(devi.getTranslation(command.getLanguage(), 384), pressure, true);
+                embed.addField(devi.getTranslation(command.getLanguage(), 385), visibility, true);
+                embed.addField(devi.getTranslation(command.getLanguage(), 386), conditions, true);
+
+                sender.reply(embed.build());
             }
-
-
-            embed.setAuthor(devi.getTranslation(command.getLanguage(), 380, city.getTitle().substring(17)));
-            embed.setFooter(devi.getTranslation(command.getLanguage(), 380, city.getTitle().substring(17)) + " | " + date, null);
-
-            embed.addField(devi.getTranslation(command.getLanguage(), 381), temp + "/" + tempF, true);
-            embed.addField(devi.getTranslation(command.getLanguage(), 382), wind, true);
-            embed.addField(devi.getTranslation(command.getLanguage(), 383), humidity, true);
-            embed.addField(devi.getTranslation(command.getLanguage(), 384), pressure, true);
-            embed.addField(devi.getTranslation(command.getLanguage(), 385), visibility, true);
-            embed.addField(devi.getTranslation(command.getLanguage(), 386), conditions, true);
-
-            sender.reply(embed.build());
         } catch (IOException | JAXBException ex) {
-            sender.reply(DeviEmote.ERROR.get() + " | " + devi.getTranslation(command.getLanguage(), 217));
+            sender.reply(DeviEmote.ERROR.get() + " | " + devi.getTranslation(command.getLanguage(), 217) + ".");
         } catch (IndexOutOfBoundsException ioobe) {
-            sender.reply(DeviEmote.ERROR.get() + " | " + devi.getTranslation(command.getLanguage(), 387));
+            sender.reply(DeviEmote.ERROR.get() + " | " + devi.getTranslation(command.getLanguage(), 387) + ".");
         }
     }
 

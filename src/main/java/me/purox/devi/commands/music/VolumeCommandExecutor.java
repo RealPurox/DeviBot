489a592196c08db0e88299a998c63ca7026fa1fa
Purox <daniel_majer@outlook.de>
2018-07-31 21:12:53 +0200

    volume

--- src/main/java/me/purox/devi/commands/music/VolumeCommandExecutor.java
+++ src/main/java/me/purox/devi/commands/music/VolumeCommandExecutor.java
@@ -16,6 +16,8 @@ import net.dv8tion.jda.core.events.message.react.MessageReactionRemoveEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
 import net.jodah.expiringmap.ExpirationPolicy;
 import net.jodah.expiringmap.ExpiringMap;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.util.AbstractMap;
 import java.util.HashMap;
@@ -26,25 +28,16 @@ import java.util.concurrent.TimeUnit;
 public class VolumeCommandExecutor implements CommandExecutor {
 
     private Devi devi;
-    //message id -> message, volume
-    private ExpiringMap<String, Map.Entry<Message, Integer>> volumeMap;
 
     public VolumeCommandExecutor(Devi devi) {
         this.devi = devi;
-        this.volumeMap = ExpiringMap.builder().variableExpiration().build();
     }
 
     @Override
     public void execute(String[] args, Command command, CommandSender sender) {
         GuildPlayer guildPlayer = devi.getMusicManager().getGuildPlayer(command.getEvent().getGuild());
         if (args.length == 0) {
-            sender.reply(getVolumeEmbed(guildPlayer), message -> {
-                if (!MessageUtils.addReactions(message, "\u2795", "\u2796")) {
-                    sender.reply(":warning: | I don't have permission to add reactions to messages, please use {0} to change the volume.".replace("{0}", "`"+ command.getPrefix() + "volume <1 - 150>`"));
-                    return;
-                }
-                volumeMap.put(message.getId(), new AbstractMap.SimpleEntry<>(message, guildPlayer.getAudioPlayer().getVolume()), ExpirationPolicy.ACCESSED, 5, TimeUnit.MINUTES);
-            });
+            sender.reply(getVolumeEmbed(guildPlayer));
         }
     }
 
@@ -63,7 +56,7 @@ public class VolumeCommandExecutor implements CommandExecutor {
         progressBar.append("**~~ ");
 
         builder.setDescription("`" + guildPlayer.getAudioPlayer().getVolume() + "` " + progressBar.toString() + " `150`");
-        builder.setFooter("Use the reactions below to change the volume", null);
+        builder.setFooter("Use !volume <1 - 150> to change the volume", null);
         return builder.build();
     }
 

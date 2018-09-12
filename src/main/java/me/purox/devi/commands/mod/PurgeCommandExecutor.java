91cfc005e467c97dacce6b1bf7c50b6460efeb92
Purox <daniel_majer@outlook.de>
2018-09-12 21:19:10 +0200

    purge rewrite

--- src/main/java/me/purox/devi/commands/mod/PurgeCommandExecutor.java
+++ src/main/java/me/purox/devi/commands/mod/PurgeCommandExecutor.java
@@ -4,15 +4,20 @@ import me.purox.devi.commands.handler.Command;
 import me.purox.devi.commands.handler.CommandExecutor;
 import me.purox.devi.commands.handler.CommandSender;
 import me.purox.devi.core.Devi;
+import me.purox.devi.core.Emote;
 import me.purox.devi.core.ModuleType;
 import net.dv8tion.jda.core.Permission;
 import net.dv8tion.jda.core.entities.Message;
 import net.dv8tion.jda.core.entities.TextChannel;
+import net.jodah.expiringmap.ExpirationPolicy;
 
-import java.util.ArrayList;
+import java.time.OffsetDateTime;
 import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Consumer;
+import java.util.stream.Collectors;
 
 public class PurgeCommandExecutor implements CommandExecutor {
 
@@ -24,8 +29,12 @@ public class PurgeCommandExecutor implements CommandExecutor {
     @Override
     public void execute(String[] args, Command command, CommandSender sender) {
         if (args.length < 1) {
-           sender.reply(devi.getTranslation(command.getLanguage(), 12, "`" + command.getPrefix()+ "purge <messages>`"));
+           sender.reply(Emote.ERROR + " | " + devi.getTranslation(command.getLanguage(), 12, "`" + command.getPrefix()+ "purge <messages>`"));
            return;
+        }
+
+        if (!command.getEvent().getGuild().getSelfMember().hasPermission(Permission.MESSAGE_MANAGE, Permission.MESSAGE_HISTORY)) {
+            sender.reply(Emote.ERROR + " | " + devi.getTranslation(command.getLanguage(), 614));
         }
 
         int amount;
@@ -37,63 +46,87 @@ public class PurgeCommandExecutor implements CommandExecutor {
 
         //limit to 500
         if (amount > 500 || amount < 1) {
-            sender.reply("//TODO EDIT LATER => Limit extended");
+            sender.reply(Emote.ERROR + " | " + devi.getTranslation(command.getLanguage(), 616));
             return;
         }
 
-        retrieveHistory(command.getEvent().getTextChannel(), command.getEvent().getMessageId(), amount, messages -> {
-                sender.reply("SUCCESS");
-        }, failure -> sender.reply("FAILURE: " + failure.getMessage()));
+        int finalAmount = amount;
+        deleteMessages(command.getEvent().getTextChannel(), command.getEvent().getMessageId(), amount, new DeleteResult(), result ->
+                sender.reply(Emote.SUCCESS + " | " + devi.getTranslation(command.getLanguage(), 153, result.getAmount())
+                        + (result.isAllDeletedSuccessfully() ? "" : "\n\n" + devi.getTranslation(command.getLanguage(), 615))),
+                failure -> sender.reply(Emote.ERROR + " | " + devi.getTranslation(command.getLanguage(), 154, finalAmount)));
     }
 
-    //TODO CLEAN THIS FUCKING MESS
-    private void retrieveHistory(TextChannel channel, String firstId, int amount, Consumer<? super Void> completed, Consumer<? super Throwable> failure) {
-        List<Message> retrieved = new ArrayList<>();
-        //100
-        channel.getHistoryBefore(firstId, 100)
-                .queue(messageHistory1 -> {
-                    retrieved.addAll(messageHistory1.getRetrievedHistory());
+    private class DeleteResult {
 
-                    if (amount <= 100) {
-                        channel.deleteMessages(retrieved).queue(completed, failure);
+        private int amount;
+        private boolean allDeletedSuccessfully;
+
+        DeleteResult() {
+            this.amount = 0;
+            this.allDeletedSuccessfully = false;
+        }
+
+        int getAmount() {
+            return amount;
+        }
+
+        boolean isAllDeletedSuccessfully() {
+            return allDeletedSuccessfully;
+        }
+
+        void setAllDeletedSuccessfully() {
+            this.allDeletedSuccessfully = true;
+        }
+
+        void increaseAmount(int increment) {
+            this.amount += increment;
+        }
+    }
+
+    private void deleteMessages(TextChannel channel, String firstId, int amount, DeleteResult deleteResult, Consumer<? super DeleteResult> completed, Consumer<? super Throwable> failure) {
+        if (amount == 1) {
+            channel.getHistoryBefore(firstId, 1).queue(messageHistory -> {
+                if (messageHistory.getRetrievedHistory().isEmpty()) {
+                    completed.accept(deleteResult);
+                    return;
+                }
+                messageHistory.getRetrievedHistory().get(0).delete().queue((o) -> {
+                    deleteResult.setAllDeletedSuccessfully();
+                    deleteResult.increaseAmount(amount);
+                    completed.accept(deleteResult);
+                }, failure);
+            }, failure);
+            return;
+        }
+
+        AtomicInteger atomicAmount = new AtomicInteger(amount);
+        channel.getHistoryBefore(firstId, amount > 100 ? 100 : amount)
+                .queue(messageHistory -> {
+                    List<Message> retrieved = messageHistory.getRetrievedHistory().stream()
+                            .filter(message -> OffsetDateTime.now().minusWeeks(2).isBefore(message.getCreationTime()))
+                            .collect(Collectors.toList());
+                    retrieved.forEach(message -> devi.getPrunedMessages().put(message.getId(), "", ExpirationPolicy.CREATED, 5, TimeUnit.MINUTES));
+                    atomicAmount.set(atomicAmount.get() - 100);
+
+                    deleteResult.increaseAmount(retrieved.size());
+
+                    if (retrieved.isEmpty()) {
+                        completed.accept(deleteResult);
+                        return;
                     }
-                    //200
-                    else
-                        channel.getHistoryBefore(retrieved.get(retrieved.size() - 1).getId(), 100)
-                                .queue(messageHistory2 -> {
-                                    retrieved.addAll(messageHistory2.getRetrievedHistory());
 
-                                    if (amount <= 200) {
-                                        channel.deleteMessages(retrieved).queue(completed, failure);
-                                    }
-                                    //300
-                                    else
-                                        channel.getHistoryBefore(retrieved.get(retrieved.size() - 1), 100)
-                                                .queue(messageHistory3 -> {
-                                                    retrieved.addAll(messageHistory3.getRetrievedHistory());
+                    //we have 100 or less messages to delete so we can use our beautiful callbacks
+                    if (atomicAmount.get() <= 0) {
+                        deleteResult.setAllDeletedSuccessfully();
+                        channel.deleteMessages(retrieved).queue((o) -> completed.accept(deleteResult), failure);
+                        return;
+                    }
 
-                                                    if (amount <= 300) {
-                                                        channel.deleteMessages(retrieved).queue(completed, failure);
-                                                    }
-                                                    //400
-                                                    else
-                                                        channel.getHistoryBefore(retrieved.get(retrieved.size() - 1), 100)
-                                                            .queue(messageHistory4 -> {
-                                                                retrieved.addAll(messageHistory4.getRetrievedHistory());
-
-                                                                if (amount <= 400) {
-                                                                    channel.deleteMessages(retrieved).queue(completed, failure);
-                                                                }
-                                                                //500
-                                                                else
-                                                                    channel.getHistoryBefore(retrieved.get(retrieved.size() -1 ), 100)
-                                                                        .queue(messageHistory5 -> {
-                                                                            retrieved.addAll(messageHistory5.getRetrievedHistory());
-                                                                            channel.deleteMessages(retrieved).queue(completed, failure);
-                                                                        });
-                                                            });
-                                                });
-                                });
+                    //more messages = more work :rolling_eyes:
+                    //not really but yeah
+                    channel.deleteMessages(retrieved).queue((o) -> deleteMessages(channel, retrieved.get(retrieved.size() - 1).getId(),
+                            atomicAmount.get(), deleteResult, completed, failure), failure);
                 }, failure);
     }
 

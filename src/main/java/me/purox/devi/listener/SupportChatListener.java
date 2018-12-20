3ca47b7377ab53081be14aab5f61d549e81a5941
Purox <daniel_majer@outlook.de>
2018-12-20 19:40:32 +0100

    Continued with support chat

--- src/main/java/me/purox/devi/listener/SupportChatListener.java
+++ src/main/java/me/purox/devi/listener/SupportChatListener.java
@@ -2,12 +2,15 @@ package me.purox.devi.listener;
 
 import me.purox.devi.core.Devi;
 import me.purox.devi.entities.supportchat.SupportChat;
-import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+import me.purox.devi.utils.MessageUtils;
+import net.dv8tion.jda.core.EmbedBuilder;
+import net.dv8tion.jda.core.entities.Message;
+import net.dv8tion.jda.core.entities.MessageEmbed;
+import net.dv8tion.jda.core.entities.User;
 import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.core.events.message.priv.PrivateMessageReceivedEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
 
-import java.util.HashMap;
 
 public class SupportChatListener extends ListenerAdapter {
 
@@ -19,19 +22,39 @@ public class SupportChatListener extends ListenerAdapter {
 
     @Override
     public void onGuildMessageReceived(GuildMessageReceivedEvent event) {
-        for (SupportChat supportChat : devi.getSupportChats()) {
-            if (supportChat.getChannel().equals(event.getChannel().getId())) {
-
+        if (event.getAuthor().isBot()) return;
+         for (SupportChat supportChat : devi.getSupportChats()) {
+            if (supportChat.getChannel().equals(event.getChannel().getId()) &&  supportChat.getStaff().equalsIgnoreCase(event.getAuthor().getId())) {
+                MessageUtils.sendPrivateMessageAsync(devi.getShardManager().getUserById(supportChat.getUser()), getEmbed(event.getMessage(), event.getAuthor()), msg -> event.getMessage().addReaction("✅").queue());
+                break;
             }
         }
     }
 
     @Override
     public void onPrivateMessageReceived(PrivateMessageReceivedEvent event) {
+        if (event.getAuthor().isBot()) return;
         for (SupportChat supportChat : devi.getSupportChats()) {
             if (supportChat.getUser().equals(event.getAuthor().getId())) {
-                
+                MessageUtils.sendMessageAsync(devi.getStaffGuild().getTextChannelById(supportChat.getChannel()), getEmbed(event.getMessage(), event.getAuthor()), msg -> event.getMessage().addReaction("✅").queue());
+                break;
             }
         }
+    }
+
+    private MessageEmbed getEmbed(Message message, User author) {
+        EmbedBuilder builder = new EmbedBuilder();
+        builder.setColor(devi.getColor());
+        builder.setAuthor("Support Chat");
+        builder.setDescription(message.getContentRaw());
+        for (Message.Attachment attachment : message.getAttachments()) {
+            if (attachment.isImage()) {
+                builder.setImage(attachment.getUrl());
+                break;
+            }
+        }
+        builder.setFooter(author.getName() + "#" + author.getDiscriminator(), author.getAvatarUrl());
+
+        return builder.build();
     }
 }

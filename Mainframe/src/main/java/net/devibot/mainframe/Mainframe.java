b3d7226d9c51a62043d65990d2427a42576326ab
Purox <daniel_majer@outlook.de>
2018-12-24 16:07:00 +0100

    setup maven and grpc

---
+++ Mainframe/src/main/java/net/devibot/mainframe/Mainframe.java
@@ -0,0 +1,14 @@
+package net.devibot.mainframe;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Mainframe {
+
+    private static Logger logger = LoggerFactory.getLogger(Mainframe.class);
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ Provider/src/main/java/net/devibot/provider/Provider.java
@@ -0,0 +1,14 @@
+package net.devibot.provider;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Provider {
+
+    private static Logger logger = LoggerFactory.getLogger(Provider.class);
+
+    public static void main(String[] args) {
+
+    }
+
+}
